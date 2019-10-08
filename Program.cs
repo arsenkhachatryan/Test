e3297b758d5ab18d3562430fd8018416f324713f
Arsen Khachatryan <arsen@rubrikkgroup.com>
2019-10-08 14:33:27 +0200

    added a test commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from master 1");
+            //Testing
+            Console.WriteLine("Hello World from master 2");
+        }
+    }
+}
